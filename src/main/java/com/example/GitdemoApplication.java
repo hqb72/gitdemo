96d851f5cfa1d9e5ff4522bdabe0e16015beec50
hqb72 <1215751114@qq.com>
2019-05-11 15:37:36 +0800

    q

---
+++ src/main/java/com/example/GitdemoApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+
+    @GetMapping("test")
+    public String test(String arg) {
+        System.out.println("arg:" + arg);
+        return StringUtils.isEmpty(arg) ? "啊?谁叫我!" : arg.replaceAll("[?吗呀哟嘛]", "!").replaceAll("[!]+","!");
+    }
+
+}
